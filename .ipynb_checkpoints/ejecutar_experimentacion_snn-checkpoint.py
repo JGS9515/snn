a4a090c7bc6882f4dd11cdbdb4da9346e15c96d3
iago-creator <iagoxabier.vazquez@gmail.com>
2024-11-29 14:26:46 +0100

    Hecha la recurrencia como opcional, modificación en inicialización de pesos (comentada).

--- .ipynb_checkpoints/ejecutar_experimentacion_snn-checkpoint.py
+++ .ipynb_checkpoints/ejecutar_experimentacion_snn-checkpoint.py
@@ -1,24 +1,55 @@
-import torch, pandas as pd, numpy as np
+import torch, pandas as pd, numpy as np, os
 from bindsnet.network import Network
 from bindsnet.network.nodes import Input, LIFNodes,AdaptiveLIFNodes
 from bindsnet.network.topology import Connection
 from bindsnet.network.monitors import Monitor
 from bindsnet.analysis.plotting import plot_spikes, plot_voltages
 from bindsnet.learning import PostPre
-from datetime import datetime
 
+#Código ppal para lanzar la experimentación para la detección de anomalías con bindsnet y STDP.
+#Este código se usaría como base para iterar sobre las distintas combinaciones de parámetros.
+#'nuu.csv'#
+path='datasets/iops/preliminar/train_procesado/1c35dbf57f55f5e4'
 
-#Archivo que contiene las funciones que se emplearán en el procesamiento con las SNNs.
+#Establecemos valores para los parámetros que nos interesan:
+nu1_pre=0.1 #Actualización de pesos presinápticos en la capa A. Valores positivos penalizan y negativos excitan.
+nu1_post=-0.1 #Actualización de pesos postsinápticos en la capa A. Valores postivos excitan y negativos penalizan.
 
-def reset_voltajes(network,device):
-    #Función que realiza el reseteo de los voltajes de las neuronas.
-    network.layers['B'].v=torch.full(network.layers['B'].v.shape,-65).to(device)
+nu2_pre=0.1 #Actualización de pesos presinápticos en la capa B. Valores positivos penalizan y negativos excitan.
+nu2_post=-0.1 #Actualización de pesos postsinápticos en la capa B. Valores postivos excitan y negativos penalizan.
+
+#Parámetros que definen la amplitud del rango de cuantiles.
+#La idea es que el valor mínimo para la codificación sea inferior al mínimo de los datos de entrenamiento, por un margen. El valor máximo debe ser también  mayor que el máximo de los datos por un margen.
+#Para ello, nos inventamos la variable a, que será la proporción del rango de datos de entrenamiento que inflamos por encima y por debajo:
+a=0.1
+#La resolución, r, indica cuán pequeños tomamos los rangos al codificar:
+r=0.01
+
+#Número de neuronas en la capa B.
+n=1000
+
+#Umbral de disparo de las neuronas LIF:
+umbral=-52
+
+#Decaimiento, en tiempo, de las neuronas LIF:
+decaimiento=100
+
+T = 1000 #Tiempo de exposición. Puede influir por la parte del entrenamiento, en la inferencia no porque los voltajes se conservan.
+#Usar el máximo de T para evitar problemas con los periodos de datos.
+expansion=100
+
+#Construimos las tuplas n1 y n2 para pasar al modelo:
+nu1=(nu1_pre,nu1_post)
+nu2=(nu2_pre,nu2_post)
+
+def reset_voltajes(network):
+    network.layers['B'].v=torch.full(network.layers['B'].v.shape,-65)
     return network
 
 
 def dividir(data,minimo):
     #Función que divide los datos de entrenamiento, para considerar aisladamente cada subsecuencia de datos normales.
-    #En principio, no es la estrategia a usar con las SNNs, pero sí será necesaria con las baselines.
+    #Tomamos los intervalos:
     intervals = []
     in_sequence = False
     
@@ -53,9 +84,6 @@ def dividir(data,minimo):
 
 
 def padd(data, T):
-    #Función que añade ceros al finalizar el conjunto de prueba, hasta llegar a un múltiplo del tiempo 
-    #de exposición.
-    
     lon = len(data)
     # Calcular el múltiplo más cercano de T superior al número actual de filas
     lon2 = ((lon // T) + 1) * T
@@ -72,7 +100,6 @@ def padd(data, T):
 
 
 def expandir(serie, n):
-    #Función que expande la serie (aunque es mejor que esta parte se haga a mano).
     # Crea gemelo de la serie:
     serie2 = np.zeros_like(serie)
     
@@ -99,7 +126,7 @@ def podar(x,q1,q2,cuantiles=None):
     return s
 
 
-def convertir_data(data,T,cuantiles,R,device,is_train=False):
+def convertir_data(data,T,cuantiles,R,is_train=False):
     #Función que lee los datos y los prepara.
     
     #Esta parte debe ser modificada para obtener la serie temporal de interés
@@ -125,7 +152,7 @@ def convertir_data(data,T,cuantiles,R,device,is_train=False):
         serie2input[i,:]=podar(serie2input[i,:],cuantiles[i],cuantiles[i+1])
     
     #Lo dividimos en función del tiempo de exposición T:
-    secuencias = torch.split(serie2input.to(device),T,dim=1)
+    secuencias = torch.split(serie2input,T,dim=1)
     
     if is_train:
         #Encaso de estar entrenando, tendríamos que quitar la última secuencia:
@@ -134,7 +161,7 @@ def convertir_data(data,T,cuantiles,R,device,is_train=False):
     return secuencias
 
 
-def crear_red(R,T,n,umbral,decaimiento,nu1,nu2,recurrencia,device):
+def crear_red():
     #Aquí creamos la red.
     
     network = Network()
@@ -155,27 +182,24 @@ def crear_red(R,T,n,umbral,decaimiento,nu1,nu2,recurrencia,device):
         source=source_layer,
         target=target_layer,
         w=0.05 + 0.1 * torch.randn(source_layer.n, target_layer.n),
-        #w=100 + 100 * torch.randn(source_layer.n, target_layer.n) #Poner parámetros más altos aquí, como estos valores de 100, ayuda a que se generen más spikes.
-        update_rule=PostPre, nu=nu1
+        update_rule=PostPre, nu=nu1#nu=(1e-4, 1e-2)    # Normal(0.05, 0.01) weights.
     )
     
     network.add_connection(
         connection=forward_connection, source="A", target="B"
     )
-    if recurrencia: #Aquí decidimos si metemos o no la capa recurrente.
-        # Creamos la conexión recurrente con pesos ligeramente negativos (si no, estamos metiendo posible ruido en el procesamiento de la red):
-        recurrent_connection = Connection(
-            source=target_layer,
-            target=target_layer,
-            w=0.025 * (torch.eye(target_layer.n) - 1),
-            update_rule=PostPre, nu=nu2#nu=(1e-4, 1e-2)
-        )
-        
-        network.add_connection(
-            connection=recurrent_connection, source="B", target="B"
-        )
-
-    network=network.to(device)
+    
+    # Creamos la conexión recurrente con pesos ligeramente negativos (si no, estamos metiendo posible ruido en el procesamiento de la red):
+    recurrent_connection = Connection(
+        source=target_layer,
+        target=target_layer,
+        w=0.025 * (torch.eye(target_layer.n) - 1), 
+        update_rule=PostPre, nu=nu2#nu=(1e-4, 1e-2)
+    )
+    
+    network.add_connection(
+        connection=recurrent_connection, source="B", target="B"
+    )
     
     #Creamos los monitores. Sirven para registrar los spikes y voltajes:
     #Spikes de entrada (para depurar que se esté haciendo bien, si se quiere):
@@ -183,14 +207,12 @@ def crear_red(R,T,n,umbral,decaimiento,nu1,nu2,recurrencia,device):
         obj=source_layer,
         state_vars=("s",),  #Registramos sólo los spikes.
         time=T,
-        device=device,
     )
     #Spikes de la capa recurrente (lo que nos interesa):
     target_monitor = Monitor(
         obj=target_layer,
         state_vars=("s", "v"),  #Registramos spikes y voltajes, por si nos interesa lo segundo también.
         time=T,
-        device=device,
     )
     
     network.add_monitor(monitor=source_monitor, name="X")
@@ -213,11 +235,9 @@ def ejecutar_red(secuencias,network,source_monitor,target_monitor,T):
         #Los datos de entrada serán una tupla con tensores de pytorch, pasamos cada una:
         print(f'Ejecutando secuencia {j}')
         j+=1
-        inputs={'A':i.T}#.to(device)
-        inicio=datetime.now()
+        inputs={'A':i.T}
         network.run(inputs=inputs, time=T)
-        final=datetime.now()
-        print(final-inicio)
+        
         #Obtenemos los spikes a lo largo de la simulación:
         spikes = {
             "X": source_monitor.get("s"), "B": target_monitor.get("s")
@@ -225,14 +245,93 @@ def ejecutar_red(secuencias,network,source_monitor,target_monitor,T):
         sp0.append(spikes['X'].sum(axis=2))
         sp1.append(spikes['B'].sum(axis=2))
         voltages = {"Y": target_monitor.get("v")}
+        #Reseteo voltajes, venga:
+        network=reset_voltajes(network)
         
     
     #Concatenamos y devolvemos:
     sp0=torch.concatenate(sp0)
-    sp0=sp0.detach().cpu().numpy()
+    sp0=sp0.detach().numpy()
     
     sp1=torch.concatenate(sp1)
-    sp1=sp1.detach().cpu().numpy()
+    sp1=sp1.detach().numpy()
     
     return [sp0,sp1,network]
+
+
+#Lectura de datos:
+#Esperamos que estos datos tengan las columnas 'label' y 'value'.
+
+data=pd.read_csv(path,na_values=['NA'])
+
+#Asegurarse de que los tipos sean correctos:
+data['value']=data['value'].astype('float64')
+data['label']=data['label'].astype('Int64')
+
+#Y ponemos a 0 los valores nulos del label para no tener problemas al filtrar por esta columna:
+data.loc[data['label'].isna(),'label']=0
+
+split = len(data) // 2
+
+data_train=data[:split]
+data_test=data[split:]
+
+#Reseteamos el índice:
+data_train = data_train.reset_index(drop=True)
+data_test = data_test.reset_index(drop=True)
+
+#Expandimos:
+data_train['label']=expandir(data_train['label'],expansion)
+
+#Sacamos máximos y mínimos:
+minimo=min(data_train['value'][data_train['label']!=1])
+maximo=max(data_train['value'][data_train['label']!=1])
+
+#Declaramos el vector de cuantiles. Para ello, tomamos el máximo y mínimo de los datos de entrenamiento (esto hay que sacarlo de esos datos)
+
+amplitud=maximo-minimo
+cuantiles=torch.FloatTensor(np.arange(minimo-a*amplitud,maximo+amplitud*a,(maximo-minimo)*r))
+
+#Ahora, establecemos el valor de R, que será el número de neuronas de la capa de entrada:
+R=len(cuantiles)-1
+
+#Crea la red.
+network, source_monitor,target_monitor = crear_red()
+
+#Dividimos el train en secuencias:
+data_train=dividir(data_train,T)
+
+#Paddeamos el test:
+data_test=padd(data_test,T)
+
+#En este punto, entrenamos para cada secuencia consecutiva del train:
+
+#Para cada secuencia del train, tenemos que pasarla y entrenar la red:
+network.learning=True
+
+for s in data_train:
+    secuencias2train=convertir_data(s,T,cuantiles,R,is_train=True)
+    print(f'Longitud de dataset de entrenamiento: {len(secuencias2train)}')
+    spikes_input,spikes,network=ejecutar_red(secuencias2train,network,source_monitor,target_monitor,T)
+    #Reseteamos los voltajes:
+    network=reset_voltajes(network)
+
+#Ahora, el test:
+network.learning=False
+secuencias2test=convertir_data(data_test,T,cuantiles,R)
+
+print(f'Longitud de dataset de prueba: {len(secuencias2test)}')
+spikes_input,spikes,network=ejecutar_red(secuencias2test,network,source_monitor,target_monitor,T)
+
+np.savetxt('spikes',spikes,delimiter=',')
+
+np.savetxt('label',np.array(data_test['label']),delimiter=',')
+
+np.savetxt('value',np.array(data_test['value']),delimiter=',')
+
+with open('n1','w') as n1:
+    n1.write(f'{R}\n')
+
+with open('n2','w') as n2:
+    n2.write(f'{n}\n')
 
