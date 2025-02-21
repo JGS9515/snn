a35ad28ef4ad46f1c56c93915016758115a5772a
Javier <javiergs9515@gmail.com>
2025-02-21 10:17:40 +0100

    The experiment is well executed but optuna throws error at the end

--- javi/ejecutar_experimento_javi.py
+++ javi/ejecutar_experimento_javi.py
@@ -7,109 +7,141 @@ from bindsnet.analysis.plotting import plot_spikes, plot_voltages
 from bindsnet.learning import PostPre
 import torch.nn.functional as F
 
+import argparse
+import json
+import optuna
+
 import numpy as np
 
 from utils import *
 
-#Código ppal para lanzar la experimentación para la detección de anomalías con bindsnet y STDP.
-#Este código se usaría como base para iterar sobre las distintas combinaciones de parámetros.
-#'nuu.csv'#
-path='Nuevos datasets/iops/preliminar/train_procesado_javi/1c35dbf57f55f5e4_filled.csv'
-# path='Nuevos datasets/Callt2/preliminar/train_label_filled.csv'
+def experiment(nu1_pre, nu1_post, nu2_pre, nu2_post, a, r, n, threshold, decay, T, expansion, path):
+    nu1=(nu1_pre,nu1_post)
+    nu2=(nu2_pre,nu2_post)
 
-#Establecemos valores para los parámetros que nos interesan:
-nu1_pre=0.1 #Actualización de pesos presinápticos en la capa A. Valores positivos penalizan y negativos excitan.
-nu1_post=-0.1 #Actualización de pesos postsinápticos en la capa A. Valores postivos excitan y negativos penalizan.
+    #Lectura de datos:
+    #Esperamos que estos datos tengan las columnas 'label' y 'value'.
 
-nu2_pre=0.1 #Actualización de pesos presinápticos en la capa B. Valores positivos penalizan y negativos excitan.
-nu2_post=-0.1 #Actualización de pesos postsinápticos en la capa B. Valores postivos excitan y negativos penalizan.
+    data=pd.read_csv(path,na_values=['NA'])
 
-#Parámetros que definen la amplitud del rango de cuantiles.
-#La idea es que el valor mínimo para la codificación sea inferior al mínimo de los datos de entrenamiento, por un margen. El valor máximo debe ser también  mayor que el máximo de los datos por un margen.
-#Para ello, nos inventamos la variable a, que será la proporción del rango de datos de entrenamiento que inflamos por encima y por debajo:
-a=0.1
-#La resolución, r, indica cuán pequeños tomamos los rangos al codificar:
-r=0.05
+    #Asegurarse de que los tipos sean correctos:
+    data['value']=data['value'].astype('float64')
+    data['label']=data['label'].astype('Int64')
 
-#Número de neuronas en la capa B.
-n=200
+    #Y ponemos a 0 los valores nulos del label para no tener problemas al filtrar por esta columna:
+    data.loc[data['label'].isna(),'label']=0
 
-#Umbral de disparo de las neuronas LIF:
-threshold=-52
+    split = len(data) // 2
 
-#Decaimiento, en tiempo, de las neuronas LIF:
-decay=100
+    data_train=data[:split]
+    data_test=data[split:]
 
-T = 250 #Tiempo de exposición. Puede influir por la parte del entrenamiento, en la inferencia no porque los voltajes se conservan.
-#Usar el máximo de T para evitar problemas con los periodos de datos.
-expansion=100
+    #Reseteamos el índice:
+    data_train = data_train.reset_index(drop=True)
+    data_test = data_test.reset_index(drop=True)
 
-#Construimos las tuplas n1 y n2 para pasar al modelo:
-nu1=(nu1_pre,nu1_post)
-nu2=(nu2_pre,nu2_post)
+    #Expandimos:
+    data_train['label']=expandir(data_train['label'],expansion)
 
-#Lectura de datos:
-#Esperamos que estos datos tengan las columnas 'label' y 'value'.
+    #Sacamos máximos y mínimos:
+    minimo=min(data_train['value'][data_train['label']!=1])
+    maximo=max(data_train['value'][data_train['label']!=1])
 
-data=pd.read_csv(path,na_values=['NA'])
+    #Declaramos el vector de cuantiles. Para ello, tomamos el máximo y mínimo de los datos de entrenamiento (esto hay que sacarlo de esos datos)
 
-#Asegurarse de que los tipos sean correctos:
-data['value']=data['value'].astype('float64')
-data['label']=data['label'].astype('Int64')
+    amplitud=maximo-minimo
+    cuantiles=torch.FloatTensor(np.arange(minimo-a*amplitud,maximo+amplitud*a,(maximo-minimo)*r))
 
-#Y ponemos a 0 los valores nulos del label para no tener problemas al filtrar por esta columna:
-data.loc[data['label'].isna(),'label']=0
+    #Ahora, establecemos el valor de snn_input_layer_neurons_size, que será el número de neuronas de la capa de entrada:
+    snn_input_layer_neurons_size=len(cuantiles)-1
 
-split = len(data) // 2
+    #Crea la red.
+    network, source_monitor, target_monitor, conv_monitor = crear_red(snn_input_layer_neurons_size,decay,threshold,nu1,nu2,n,T)
 
-data_train=data[:split]
-data_test=data[split:]
+    #Dividimos el train en secuencias:
+    data_train=dividir(data_train,T)
 
-#Reseteamos el índice:
-data_train = data_train.reset_index(drop=True)
-data_test = data_test.reset_index(drop=True)
+    #Paddeamos el test:
+    data_test=padd(data_test,T)
 
-#Expandimos:
-data_train['label']=expandir(data_train['label'],expansion)
+    #En este punto, entrenamos para cada secuencia consecutiva del train:
 
-#Sacamos máximos y mínimos:
-minimo=min(data_train['value'][data_train['label']!=1])
-maximo=max(data_train['value'][data_train['label']!=1])
+    #Para cada secuencia del train, tenemos que pasarla y entrenar la red:
+    network.learning=True
 
-#Declaramos el vector de cuantiles. Para ello, tomamos el máximo y mínimo de los datos de entrenamiento (esto hay que sacarlo de esos datos)
+    for s in data_train:
+        secuencias2train=convertir_data(s,T,cuantiles,snn_input_layer_neurons_size,is_train=True)
+        print(f'Longitud de dataset de entrenamiento: {len(secuencias2train)}')
+        spikes_input,spikes,spikes_conv,network=ejecutar_red(secuencias2train,network,source_monitor,target_monitor,conv_monitor,T)
+        #Reseteamos los voltajes:
+        network=reset_voltajes(network)
 
-amplitud=maximo-minimo
-cuantiles=torch.FloatTensor(np.arange(minimo-a*amplitud,maximo+amplitud*a,(maximo-minimo)*r))
+    #Ahora, el test:
+    network.learning=False
+    secuencias2test=convertir_data(data_test,T,cuantiles,snn_input_layer_neurons_size)
 
-#Ahora, establecemos el valor de snn_input_layer_neurons_size, que será el número de neuronas de la capa de entrada:
-snn_input_layer_neurons_size=len(cuantiles)-1
+    print(f'Longitud de dataset de prueba: {len(secuencias2test)}')
+    spikes_input,spikes,spikes_conv,network=ejecutar_red(secuencias2test,network,source_monitor,target_monitor,conv_monitor,T)
 
-#Crea la red.
-network, source_monitor, target_monitor, conv_monitor = crear_red(snn_input_layer_neurons_size,decay,threshold,nu1,nu2,n,T)
+    guardar_resultados(spikes,spikes_conv,data_test,n,snn_input_layer_neurons_size)
+    
 
-#Dividimos el train en secuencias:
-data_train=dividir(data_train,T)
+def objective(trial):
 
-#Paddeamos el test:
-data_test=padd(data_test,T)
+    config = {
+        'nu1': trial.suggest_float('nu1', -0.5, 0.5),
+        'nu2': trial.suggest_float('nu2', -0.5, 0.5),
+        'threshold': trial.suggest_float('threshold', 0.1, 1.0),
+        'decay': trial.suggest_float('decay', 80, 150),
+    }
+    print(f"config: {config}")
 
-#En este punto, entrenamos para cada secuencia consecutiva del train:
+    #Establecemos valores para los parámetros que nos interesan:
+    nu1_pre=0.1 #Actualización de pesos presinápticos en la capa A. Valores positivos penalizan y negativos excitan.
+    nu1_post=-0.1 #Actualización de pesos postsinápticos en la capa A. Valores postivos excitan y negativos penalizan.
 
-#Para cada secuencia del train, tenemos que pasarla y entrenar la red:
-network.learning=True
+    nu2_pre=0.1 #Actualización de pesos presinápticos en la capa B. Valores positivos penalizan y negativos excitan.
+    nu2_post=-0.1 #Actualización de pesos postsinápticos en la capa B. Valores postivos excitan y negativos penalizan.
 
-for s in data_train:
-    secuencias2train=convertir_data(s,T,cuantiles,snn_input_layer_neurons_size,is_train=True)
-    print(f'Longitud de dataset de entrenamiento: {len(secuencias2train)}')
-    spikes_input,spikes,spikes_conv,network=ejecutar_red(secuencias2train,network,source_monitor,target_monitor,conv_monitor,T)
-    #Reseteamos los voltajes:
-    network=reset_voltajes(network)
+    #Parámetros que definen la amplitud del rango de cuantiles.
+    #La idea es que el valor mínimo para la codificación sea inferior al mínimo de los datos de entrenamiento, por un margen. El valor máximo debe ser también  mayor que el máximo de los datos por un margen.
+    #Para ello, nos inventamos la variable a, que será la proporción del rango de datos de entrenamiento que inflamos por encima y por debajo:
+    a=0.1
+    #La resolución, r, indica cuán pequeños tomamos los rangos al codificar:
+    r=0.05
 
-#Ahora, el test:
-network.learning=False
-secuencias2test=convertir_data(data_test,T,cuantiles,snn_input_layer_neurons_size)
+    #Número de neuronas en la capa B.
+    n=200
 
-print(f'Longitud de dataset de prueba: {len(secuencias2test)}')
-spikes_input,spikes,spikes_conv,network=ejecutar_red(secuencias2test,network,source_monitor,target_monitor,conv_monitor,T)
+    #Umbral de disparo de las neuronas LIF:
+    threshold=-52
 
-guardar_resultados(spikes,spikes_conv,data_test,n,snn_input_layer_neurons_size)
+    #Decaimiento, en tiempo, de las neuronas LIF:
+    decay=100
+
+    T = 250 #Tiempo de exposición. Puede influir por la parte del entrenamiento, en la inferencia no porque los voltajes se conservan.
+    #Usar el máximo de T para evitar problemas con los periodos de datos.
+    expansion=100
+
+    experiment(nu1_pre, nu1_post, nu2_pre, nu2_post, a, r, n, threshold, decay, T, expansion, path)
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Optimización de hiperparámetros con Optuna.')
+    path='Nuevos datasets\\iops\\preliminar\\train_procesado_javi\\1c35dbf57f55f5e4_filled.csv'
+    # path='Nuevos datasets/Callt2/preliminar\\train_label_filled.csv'
+    parser.add_argument('-d', '--data_path', type=str, default='Nuevos datasets\\Callt2\\preliminar\\train_label_filled.csv', help='Ruta al archivo de datos CSV')
+    parser.add_argument('-n', '--n_trials', type=int, default=1, help='Número de trials para Optuna')
+    args = parser.parse_args()
+
+    study = optuna.create_study(direction='maximize')
+    study.optimize(objective, n_trials=args.n_trials)
+
+    print('Mejor configuración encontrada:')
+    print(study.best_params)
+    print(f'Mejor F1-score: {study.best_value}')
+
+    # Guardar la mejor configuración
+    with open('best_config.json', 'w') as f:
+        json.dump(study.best_params, f, indent=4)
